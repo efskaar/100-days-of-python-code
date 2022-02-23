529bf7ee445aa055b4c9377ef013b8f7b627128f
efskaar <59534768+efskaar@users.noreply.github.com>
2022-02-23 10:37:24 +0100

    Update 036cookieClickerBot.py
    Updated to a working coockie clicker bot :D

--- 036cookieClickerBot.py
+++ 036cookieClickerBot.py
@@ -1,41 +1,52 @@
-from time import sleep
-import os
-from matplotlib.style import available
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
 
-chrome_driver_path = ""
+chrome_driver_path = "C:/Users/skaar/.wdm/drivers/chromedriver/win32/98.0.4758.102/chromedriver.exe"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 url = 'https://orteil.dashnet.org/cookieclicker/'
 driver.get(url)
-# print(driver.find_element(By.ID,'productTitle').text)
-# print(driver.find_element(By.CSS_SELECTOR,'.documentation-widget a').text)
-# print(driver.find_element(By.XPATH,'//*[@id="container"]/li[2]/a').click())
 cookie = driver.find_element(By.XPATH,'//*[@id="bigCookie"]')
 
 def gameLogic():
-  upgrade = driver.find_elements(By.XPATH,'//*[@id="upgrade*"]')
-  # products = driver.find_element(By.ID,'#product0')
-  # print(len(products))
-  print(len(upgrade))
-  # try:
-  #   upgrade.click()
-  # except:
-  #   None
-  # for product in products:
-  #   try:
-  #     product.click()
-  #   except:
-  #     None
+  #upgradesFirst
+  buyUpgrade()
 
-counter = 0
+  #buy products second
+  buyProducts()
+
+def buyUpgrade():
+  for i in range(3):
+    try:
+      upgrade = driver.find_element(By.XPATH,'//*[@id="upgrade0"]')
+      upgrade.click()
+    except:
+      None
+
+def buyProducts():
+  for i in range(18,-1,-1):
+    try:
+      product = driver.find_element(By.XPATH,f'//*[@id="product{i}"]')
+      for i in range(3):
+        product.click()
+    except:
+      None
+
+def clickGoldenCookie():
+  try:
+    golden_cookie = driver.find_element(By.XPATH,f'//*[@id="shimmers"]')
+    golden_cookie.click()
+  except:
+    None
+
+counter = 1
 while True:
-  if counter%100 == 0:
+  if counter%1000 == 0:
     gameLogic()
-    sleep(2)
-    os.system('cls')
+    counter = 1
+  elif counter%499 == 0:
+    clickGoldenCookie()
   else:
     cookie.click()
   counter += 1
